924ef2eb61a75bc6509fc202a84651ed3eed2168
SandeshGC <gcsandesh01@gmail.com>
2023-12-11 17:42:44 +0545

    feat: add styles to create landmark page
    - add font awesome to project
    - add tailwindcss to project
    - style create landmark form

--- gantavya/base/forms.py
+++ gantavya/base/forms.py
@@ -1,15 +1,19 @@
 from django import forms
 from .models import Photos, Landmark
+from django.forms import ModelForm, TextInput
 
-class PhotoUploadForm(forms.ModelForm):
+class PhotoUploadForm(ModelForm):
     class Meta:
         model = Photos
         fields = ['place', 'photo']
 
-
-
-
-class LandmarkForm(forms.ModelForm):
+class LandmarkForm(ModelForm):
     class Meta:
         model = Landmark
         fields = ['name', 'address', 'type', 'description']
+        widgets = {
+            'name': TextInput(attrs={'class': 'py-1 px-1 ml-auto rounded-sm my-2 border-2 border'}),
+            'address': TextInput(attrs={'class':'py-1 px-1 ml-auto rounded-sm my-2 border-2 border'}),
+            'type': TextInput(attrs={'class': 'py-1 px-1 ml-auto rounded-sm my-2 border-2 border'}),
+            'description': TextInput(attrs={'class': 'py-1 px-1 ml-auto rounded-sm my-2 border-2 border'}),
+        }
