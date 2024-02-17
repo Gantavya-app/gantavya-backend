c81c29842b860983563d07a3e8b3d87260498e97
Rupesh Ghimire <rupeshghimire007@gmail.com>
2024-02-17 23:23:32 +0545

    Removed list filters from user admin config

--- gantavya/base/admin.py
+++ gantavya/base/admin.py
@@ -6,7 +6,6 @@ from .models import Photos, Landmark, User
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'name')
     search_fields = ('username', 'email')
-    list_filter = ('isAdmin',)
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Photos)
