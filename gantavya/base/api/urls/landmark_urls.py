eb219d95e6d55c50ca23f7ca7ecdeb5f8cbbe724
Rupesh Ghimire <rupeshghimire007@gmail.com>
2024-02-22 22:00:57 +0545

    Paths for landmark created

--- gantavya/base/api/urls/landmark_urls.py
+++ gantavya/base/api/urls/landmark_urls.py
@@ -4,6 +4,13 @@ from base.api.views import landmark_view as views
 
 urlpatterns = [
     path('',views.landmark_list, name='get_all_landmarks'),
+    path('upload_photo/<str:pk>', views.upload_photo, name='photo_upload'),
+    path('delete_photo/<str:pk>', views.delete_photo, name="photo_delete"),
     
+    path('landmark/<str:pk>', views.landmark_detail, name='landmark'),
+    path('create/', views.create_landmark, name='create_landmark'),
+    path('saved_by/', views.save_landmark, name="saved_by"),
+
+    path('prediction/', views.prediction, name='prediction'),
 
 ]
