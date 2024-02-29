06cb596c5af1b985fefe0d02daa55e050da863fe
Rupesh Ghimire <rupeshghimire007@gmail.com>
2024-02-29 19:01:16 +0545

    URLS changed as pk/

--- gantavya/base/api/urls/landmark_urls.py
+++ gantavya/base/api/urls/landmark_urls.py
@@ -4,12 +4,12 @@ from base.api.views import landmark_view as views
 
 urlpatterns = [
     path('',views.landmark_list, name='get_all_landmarks'),
-    path('upload_photo/<str:pk>', views.upload_photo, name='photo_upload'),
-    path('delete_photo/<str:pk>', views.delete_photo, name="photo_delete"),
+    path('upload_photo/<str:pk>/', views.upload_photo, name='photo_upload'),
+    path('delete_photo/<str:pk>/', views.delete_photo, name="photo_delete"),
     
-    path('landmark/<str:pk>', views.landmark_detail, name='landmark'),
+    path('landmark/<str:pk>/', views.landmark_detail, name='landmark'),
     path('create/', views.create_landmark, name='create_landmark'),
-    path('saved_by/<str:pk>', views.save_landmark, name="saved_by"),
+    path('saved_by/<str:pk>/', views.save_landmark, name="saved_by"),
 
     path('prediction/', views.prediction, name='prediction'),
 
